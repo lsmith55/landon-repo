835d5fbcf8c364e9cc956486ebf808db56fe6e25
Landon <143634640+lsmith55@users.noreply.github.com>
2023-09-05 13:06:56 -0500

    Create practice.php

---
+++ practice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WDV341 Intro PHP</h1>
+    <H2>PHP Basics and examples</h2>
+    <?php
+        echo "<h3> This is an ouput of PHP on server</h3>;"
+    ?>
+</body>
+</html>
