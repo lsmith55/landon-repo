14dd3baf4e6d04086f974c9e3f891fd1c81ad75e
Landon <143634640+lsmith55@users.noreply.github.com>
2023-09-09 13:16:59 -0500

    updated css

--- homework.php
+++ homework.php
@@ -4,13 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landon Smith Homework Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+
+        h1 {
+            color: #333;
+            margin-top: 20px;
+        }
+
+        h3 {
+            color: #555;
+        }
+
+        p {
+            color: #777;
+        }
+
+        a {
+            text-decoration: none;
+            color: #007BFF;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
-<?php
-        echo "<h1>Welcome to my Homework Page!</h1>";
-        echo "<h3>Here are some of my links to past projects.</h3>";
-        echo "<p>Click <a href='http://landonsmith.org/wdv341/wdv341/landon-repo/terms.html'>here</a> to visit the Terms assignment.</p>";
-        echo "<p>Click <a href='http://landonsmith.org/wdv341/wdv341/landon-repo/phpbasics.php'>here</a> to visit PHP Basics Assignment 1.</p>"
+    <?php
+    echo "<h1>Welcome to my Homework Page!</h1>";
+    echo "<h3>Here are some of my links to past projects.</h3>";
+    echo "<p>Click <a href='http://landonsmith.org/wdv341/wdv341/landon-repo/terms.html'>here</a> to visit the Terms assignment.</p>";
+    echo "<p>Click <a href='http://landonsmith.org/wdv341/wdv341/landon-repo/phpbasics.php'>here</a> to visit PHP Basics Assignment 1.</p>"
     ?>
 </body>
 </html>
