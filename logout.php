e9113e3500e8c5f51d2c8df98d94f1492a7731c4
Landon <143634640+lsmith55@users.noreply.github.com>
2023-12-05 23:23:11 -0600

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+
+if (isset($_GET['db_connection'])) {
+    $conn = unserialize(urldecode($_GET['db_connection']));
+    if ($conn) {
+        // Close the database connection
+        mysqli_close($conn);
+    }
+}
+
+
+header("Location: signon.php");  
+exit();
+?>
